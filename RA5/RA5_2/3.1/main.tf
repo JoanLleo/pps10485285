448f43ea764fdf3344a4ac7138ac721cee7b6d02
Live session user <joalleebr@alu.edu.gva.es>
2025-05-19 13:14:37 +0200

    3.1

--- RA5/RA5_2/3.1/main.tf
+++ RA5/RA5_2/3.1/main.tf
@@ -1,11 +1,6 @@
 resource "null_resource" "vagrant_vm" {
   provisioner "local-exec" {
-    command = "vagrant up"
-  }
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = "vagrant destroy -f"
+    command = "vagrant up --no-provision"
   }
 
   triggers = {
